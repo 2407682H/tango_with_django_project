277a45c66af36732451ee6da6eb441e4990fa3b3
Jake Haakanson <2407682H@student.gla.ac.uk>
2020-01-23 00:20:28 +0000

    Chapter 4 completed

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! " + "<a href = '/rango/about/'>About</a>")
+    #Passed to template engine, matches {{ boldmessage }}
+    #Key value pairs employed within the template
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    #Return rendered response to send to client, first parameter is template
+    #The render function combines the template data and the context_dict to produce a page
+    return render(request, "rango/index.html", context = context_dict)
+
+    #return HttpResponse("Rango says hey there partner! " + "<a href = '/rango/about/'>About</a>")
+
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. " + "<a href = '/rango/'>Index</a>")
+    context_dict = {"boldmessage": "This tutorial has been put together by Jake Haakanson."}
+
+    return render(request, "rango/about.html", context = context_dict)
+    #return HttpResponse("Rango says here is the about page. " + "<a href = '/rango/'>Index</a>")
