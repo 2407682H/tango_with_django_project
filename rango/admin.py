4446af6739410f62c480f10f09bf28357081338c
Jake Haakanson <2407682H@student.gla.ac.uk>
2020-01-23 19:35:10 +0000

    Completed chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "url")
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+
+"""
+#From django tutorial
+from rango.models import Question, Choice
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "pub_date", "was_published_recently")
+
+    fieldsets = [
+		(None, 			{"fields": ["question_text"]}),
+		("Date Information", 	{"fields": ["pub_date"], "classes": ["collapse"]})
+    ]
+
+    inlines = [ChoiceInLine]
+
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
+"""
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,56 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+#Category class
+class Category(models.Model):
+    name = models.CharField(max_length = 128, unique = True) #Define a character field of length 128 that must be unique
+    views = models.IntegerField(default = 0)
+    likes = models.IntegerField(default = 0)
+
+    #Nested meta class to fix typo
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+      return self.name
+
+
+#Page class
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete = models.CASCADE) #Foreign key of category - OneToMany relationship
+    title = models.CharField(max_length = 128)
+    url = models.URLField()
+    views = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.title
+
+
+"""
+#Chapter 2 and 7 of Django tutorial
+#Question class
+class Question(models.Model):
+    question_text = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField("Date published")
+
+    def __str__(self):
+        return self.question_text
+
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+
+    was_published_recently.admin_order_field = "pub_date"
+    was_published_recently.boolean = True
+    was_published_recently.short_description = "Published recently?"
+#Choice class
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.choice_text
+"""
